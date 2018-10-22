c1c314b90ae45120483e81e93a40e2618f9cf14a
Matheus Fernandes <matheussky.mfj@gmail.com>
2018-10-22 01:46:00 -0300

    criadas e testadas as entidades Estado e Cidade

---
+++ src/main/java/com/mathessky/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.mathessky.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mathessky.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
